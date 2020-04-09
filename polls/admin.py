2e89b7ba877b93c7a8ddc9fb44db7ab5b84ddc10
jcardev <juanchocava@gmail.com>
2020-04-08 21:03:51 -0500

    Manual django parte 2

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import Question
 # permite que las encuestas se puedan modificar en el sitio
-admin.site.register(Question)
+admin.site.register(Question) 
